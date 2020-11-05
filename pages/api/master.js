ce1132e05fccd109772e09caedd4e2765df9194b
kurilee <sasukeliz@sina.cn>
2020-11-05 16:14:13 +0800

    测试测试4

--- pages/api/master.js
+++ pages/api/master.js
@@ -1,9 +1,12 @@
+import { get } from "http"
+
 function Master({ data }) {
     return <div>data</div>
 }
 
 export async function getServerSideProps(context) {
-    const res = await fetch('http://kuriz.oicp.io/master?pageNo=1&pageSize=30')
+    //const res = await fetch('http://kuriz.oicp.io/master?pageNo=1&pageSize=30')
+    const res = await get('http://kuriz.oicp.io/master?pageNo=1&pageSize=30')
     const data = await res.json()
   
     // Pass data to the page via props
