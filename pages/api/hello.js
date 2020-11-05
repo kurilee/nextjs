6ddcce288e84ccb597e0ed67496cbd7c9deb3fe6
kurilee <sasukeliz@sina.cn>
2020-11-05 15:49:24 +0800

    测试测试

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  const res = await fetch('http://kuriz.oicp.io/master?pageNo=1&pageSize=30');
+  res.statusCode = 200;
+  res.json(res);
 }
