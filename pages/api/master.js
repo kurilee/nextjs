09d2b5e0ed9057556536f2e5084e589dd94ff9cf
kurilee <sasukeliz@sina.cn>
2020-11-05 16:05:45 +0800

    测试测试3

---
+++ pages/api/master.js
@@ -0,0 +1,13 @@
+function Master({ data }) {
+    return <div>data</div>
+}
+
+export async function getServerSideProps(context) {
+    const res = await fetch('http://kuriz.oicp.io/master?pageNo=1&pageSize=30')
+    const data = await res.json()
+  
+    // Pass data to the page via props
+    return { props: { data } }
+}
+
+export default Master
