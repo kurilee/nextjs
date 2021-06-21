78f51f1bc9fdf4eb351f5c0abc524e4b0c316939
kurilee <sasukeliz@sina.cn>
2021-06-22 00:48:01 +0800

    更新youtube接口

--- pages/api/youtube.js
+++ pages/api/youtube.js
@@ -1,5 +1,8 @@
 export default async (req, res) => {
     const { q, nextPageToken } = req.body;
-    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${q}&nextPageToken=${nextPageToken}&maxResults=50&order=relevance&regionCode=JP&key=AIzaSyBr6tbHXG6lVecYpgYjXoLUFE_8HQilrqc`, { method: 'GET'});
-    return response.body;
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${q}&nextPageToken=${nextPageToken}&maxResults=50&order=relevance&regionCode=JP&key=AIzaSyBr6tbHXG6lVecYpgYjXoLUFE_8HQilrqc`
+    console.log(url);
+    const response = await fetch(url, { method: 'GET'});
+    console.log(response);
+    res.json(response.body);
 }  
