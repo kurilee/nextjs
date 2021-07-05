be969065850cd296410ccd41c88d9a01115eccf9
kurilee <sasukeliz@sina.cn>
2021-07-05 09:21:08 +0800

    apikey作为参数传入, 而不是写死

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
-    const { q, nextPageToken } = req.body;
-    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${q}&nextPageToken=${nextPageToken}&maxResults=50&order=relevance&type=video&regionCode=JP&key=AIzaSyBr6tbHXG6lVecYpgYjXoLUFE_8HQilrqc`
+    const { q, nextPageToken, apikey } = req.body;
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${q}&nextPageToken=${nextPageToken}&maxResults=50&order=relevance&type=video&regionCode=JP&key=${apikey}`
     const response = await fetch(url, { method: 'GET'});
     res.json(response.body);
 }  
